88777e7fd2b753123346eaf715f85697cc6b1d49
Ben Mariano <benmariano13@gmail.com>
2019-07-07 00:37:07 +0200

    build scripts for vm

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+export DEBIAN_FRONTEND=noninteractive
+
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select trusty'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.2-1_all.deb
+dpkg -i mysql-apt-config_0.7.2-1_all.deb
+apt-get update
+apt-get install -y --force-yes mysql-server libmysqlclient-dev
+mysql -u root -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"

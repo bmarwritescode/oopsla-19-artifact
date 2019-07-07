f7f84fd1a77810b6a7627dad4062830531235b96
Ben Mariano <benmariano13@gmail.com>
2019-07-07 17:35:26 +0200

    added clean scripts for results

---
+++ artifact_materials/artifact_results/clean.sh
@@ -0,0 +1,24 @@
+# Clean Full
+cd full
+./clean.sh
+
+cd ..
+
+# Clean Abridged
+cd abridged
+./clean.sh
+
+cd ..
+
+# Clean Short
+cd short
+./clean.sh
+
+cd ..
+
+# Clean Sloccounts
+cd sloccounts
+./clean.sh
+
+cd ..
+
